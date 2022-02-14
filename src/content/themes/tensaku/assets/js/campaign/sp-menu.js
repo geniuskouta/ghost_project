27ef98fae488bc4fdb662a43ebc68c053e82bd46
geniuskouta <knakano95@gmail.com>
2022-02-14 09:50:37 +0900

    add slider effect to sp nav menu on campaign page

--- src/content/themes/tensaku/assets/js/campaign/sp-menu.js
+++ src/content/themes/tensaku/assets/js/campaign/sp-menu.js
@@ -13,7 +13,7 @@ function registerToggleSpMenu() {
 
     function openMenu() {
         disableScroll();
-        fadeOpenMenu();
+        slideOpenMenu();
         overlay.addEventListener('click', closeMenu);
         Array.prototype.forEach.call(closeTriggers, function(closeTrigger) {
             closeTrigger.addEventListener('click', closeMenu);
@@ -22,34 +22,56 @@ function registerToggleSpMenu() {
 
     function closeMenu() {
         enableScroll();
-        fadeCloseMenu();
+        slideCloseMenu();
         overlay.removeEventListener('click', closeMenu);
         Array.prototype.forEach.call(closeTriggers, function(closeTrigger) {
             closeTrigger.removeEventListener('click', closeMenu);
         });
     }
 
-    function fadeOpenMenu() {
-        nav.style.opacity = 0;
+    function slideOpenMenu() {
+        // slide animation defined in CSS
         overlay.style.opacity = 0;
-        nav.classList.add(styles.navShown);
         overlay.classList.add(styles.overlayShown);
+        nav.style.display = "block";
         setTimeout(function() {
-            nav.style.opacity = "";
             overlay.style.opacity = "";
+            nav.classList.add(styles.navShown);
         }, 100);
     }
 
-    function fadeCloseMenu() {
-        nav.style.opacity = 0;
+    function slideCloseMenu() {
+        // slide animation defined in CSS
         overlay.style.opacity = 0;
+        nav.classList.remove(styles.navShown);
         setTimeout(function() {
-            nav.classList.remove(styles.navShown);
             overlay.classList.remove(styles.overlayShown);
             overlay.style.opacity = "";
-            nav.style.opacity = "";
-        }, 300);
+            nav.style.display = "";
+        }, 100);
     }
+
+    // function fadeOpenMenu() {
+    //     nav.style.opacity = 0;
+    //     overlay.style.opacity = 0;
+    //     nav.classList.add(styles.navShown);
+    //     overlay.classList.add(styles.overlayShown);
+    //     setTimeout(function() {
+    //         nav.style.opacity = "";
+    //         overlay.style.opacity = "";
+    //     }, 100);
+    // }
+
+    // function fadeCloseMenu() {
+    //     nav.style.opacity = 0;
+    //     overlay.style.opacity = 0;
+    //     setTimeout(function() {
+    //         nav.classList.remove(styles.navShown);
+    //         overlay.classList.remove(styles.overlayShown);
+    //         overlay.style.opacity = "";
+    //         nav.style.opacity = "";
+    //     }, 300);
+    // }
 
     function disableScroll() {
           var body = document.querySelector('body');
